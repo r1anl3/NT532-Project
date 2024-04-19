631581bda1e2ad1c98bffceaf0edc0e1813f88e8
r1anl3 <levu1409@gmail.com>
2024-04-19 22:54:03 +0700

    final

--- run-yolo.sh
+++ run-yolo.sh
@@ -6,6 +6,6 @@ read -p "Enter type (onboard/video/image): " type
 read -p "Enter source (id/video/image path): " source
 
 # Run the Python code with the entered arguments
-python3 trt-yolo-${num}.py --${type} "${source}" -m yolov4-416
+python3 trt_yolo_r${num}.py --${type} "${source}" -m yolov4-416
 
 echo "Done!"
