881cfa295f3fda61643627addf5e4d1f6a345f18
r1anl3 <levu1409@gmail.com>
2024-04-20 09:33:17 +0700

    no flip

--- utils/camera.py
+++ utils/camera.py
@@ -115,7 +115,7 @@ def open_cam_onboard(width, height):
                    'video/x-raw(memory:NVMM), '
                    'width=(int)1920, height=(int)1080, '
                    'format=(string)NV12, framerate=(fraction)30/1 ! '
-                   'nvvidconv flip-method=2 ! '
+                   'nvvidconv flip-method=0 ! '
                    'video/x-raw, width=(int){}, height=(int){}, '
                    'format=(string)BGRx ! '
                    'videoconvert ! appsink').format(width, height)
